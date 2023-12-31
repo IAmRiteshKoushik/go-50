ce076ea93e913e7a220138ad02eb8fbf7ddcc0c4
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-12-31 12:20:23 +0530

    done: Weather tracker project

---
+++ 10-go-weather/main.go
@@ -0,0 +1,80 @@
+package main
+
+import(
+  "encoding/json"
+  "os"
+  "net/http"
+  "strings"
+)
+
+type apiConfigData struct {
+  OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct {
+  Name string `json:"name"`
+  Main struct {
+    Kelvin float64 `json:"temp"`
+  } `json:"main"`
+}
+
+func loadApiConfig(filename string)(apiConfigData, error){
+  bytes, err := os.ReadFile(filename)
+  if err != nil {
+    return apiConfigData{}, err
+  }
+
+  // If the json is able to be unmarshalled into the apiConfigData struct
+  // then we can proceed with the operation else, an error has to be 
+  // returned.
+  var c apiConfigData
+  err = json.Unmarshal(bytes, &c)
+  if err != nil {
+    return apiConfigData{}, err
+  }
+
+  return c, nil
+}
+
+// Handler functions
+func hello(w  http.ResponseWriter, r *http.Request){
+  w.Write([]byte("Hello from Go\n"))
+}
+
+func query(city string)(weatherData, error){
+  apiConfig, err := loadApiConfig(".apiConfig")
+  if err != nil {
+    return weatherData{}, err
+  }
+
+  apiUrl := "http://api.openweathermap.org/data/2.5/weather?APPID="
+  resp, err := http.Get(apiUrl + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
+  if err != nil {
+    return weatherData{}, err
+  }
+  defer resp.Body.Close()
+
+  var d weatherData
+  if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+    return weatherData{}, err
+  }
+  return d, nil
+}
+
+// Driver program
+func main(){
+  http.HandleFunc("/hello", hello)
+  http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+    city := strings.SplitN(r.URL.Path, "/", 3)[2]
+    data, err := query(city)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    w.Header().Set("Content-type", "application/json; charset=utf-8")
+    json.NewEncoder(w).Encode(data)
+  
+  })
+
+  http.ListenAndServe(":8080", nil)
+}
