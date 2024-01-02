162b8aed385f20f6f36024ea2ca095fa5e9f8660
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-01-02 18:31:52 +0530

    add: TodoList project

---
+++ 38-go-todo/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/thedevsaddam/renderer"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var rnd *renderer.Render
+var db *mgo.Database
+
+const (
+	hostName       string = "localhost:27017" // PORT for MongoDB
+	dbName         string = "demo_todo"
+	collectionName string = "todo" 
+    port           string = ":9000"
+)
+
+type(
+  todoModel struct{
+    ID        bson.ObjectId `bson:"_id,omitempty"`
+    Title     string        `bson:"title"`
+    Completed bool          `bson:"completed"`
+    CreatedAt time.Time     `bson:"createdAt"`
+  }
+
+  todo struct {
+    ID        string    `json:"id"`
+    Title     string    `json:"title"`
+    Completed string    `json:"completed"`
+    CreatedAt time.Time `json:"created_at"`
+  }
+)
+
+func init(){
+  rnd = renderer.New()
+  sess, err := mgo.Dial(hostName)
+  checkErr(err)
+  sess.SetMode(mgo.Monotonic, true)  // Modes in MongoDB - String, Monotonic and Eventual
+  db = sess.DB(dbName)
+}
+
+func checkErr(err error){
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func todoHandlers() http.Handler {
+  rg := chi.NewRouter();
+  rg.Group(func(r chi.Router){
+    r.Get("/", fetchTodos)
+    r.Post("/", createTodo)
+    r.Put("/{id}", updateTodo)
+    r.Delete("/{id}", deleteTodo)
+  })
+  return rg
+}
+
+func main(){
+  r := chi.NewRouter();
+  r.Use(middleware.Logger)
+  r.Get("/", homeHandler)
+  r.Mount("/todo", todoHandlers())
+
+  srv := &http.Server{
+    Addr: port,
+    Handler: r,
+    ReadTimeout: 60 * time.Second,
+    WriteTimeout: 60 * time.Second,
+    IdleTimeout: 60 * time.Second,
+  }
+
+  go func(){ // goroutines
+    log.Println("Listening on port", port)
+    if err := srv.ListenAndServe(); err != nil {
+      log.Printf("Listen:%s\n", err)
+    }
+  }()
+}
