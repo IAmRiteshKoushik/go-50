64f7b02b2461629ba636bb45af3f148a52ae8db3
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-05-11 00:04:18 +0530

    Simulated MongoDB in JSON files

--- 19-go-database/main.go
+++ 19-go-database/main.go
@@ -5,7 +5,8 @@ import (
     "fmt"
     "os"
     "sync"
-    "github.com/blend/go-sdk/stringutil"
+    "path/filepath"
+
     "github.com/jcelliott/lumber" // A simple logger for Go
 )
 
@@ -46,33 +47,149 @@ type Options struct {
     Logger
 }
 
-func New()(){
+func New(dir string, options *Options) (*Driver, error){
+    dir = filepath.Clean(dir)
+    opts := Options{}
+    if options != nil {
+        opts = *options
+    }
+    if opts.Logger == nil {
+        opts.Logger = lumber.NewConsoleLogger((lumber.INFO))
+    }
+    driver := Driver{
+        dir: dir,
+        mutexes: make(map[string]*sync.Mutex),
+        log: opts.Logger,
+    }
 
+    // If database exists
+    if _, err := os.Stat(dir); err == nil {
+        opts.Logger.Debug("Using '%s' (database already exists)\n", dir)
+        return &driver, nil
+    }
+    // If database does not exist
+    opts.Logger.Debug("Creating the database at '%s' ...\n", dir)
+    return &driver, os.MkdirAll(dir, 0755)
 }
 
-func (d *Driver) Write() error {
+func (d *Driver) Write(collection, resource string, v interface{}) error {
+    // the collection is the directory
+    // the resource is the file name
+    if collection == ""{
+        return fmt.Errorf("Missing collection - no place to save record!")
+    }    
+    if resource == "" {
+        return fmt.Errorf("Missing resource - unable to save record (no name)!")
+    }
 
+    mutex := d.getOrCreateMutex(collection)
+    mutex.Lock()
+    defer mutex.Unlock()
+
+    dir := filepath.Join(d.dir, collection)
+    finalPath := filepath.Join(dir, resource + ".json")
+    tmpPath := finalPath + ".tmp"
+
+    if err := os.MkdirAll(dir, 0755); err != nil {
+        return err
+    }
+    b, err := json.MarshalIndent(v, "", "\t")
+    if err != nil {
+        return err
+    }
+
+    b = append(b, byte('\n'))
+    if err := os.WriteFile(tmpPath, b, 0644); err != nil {
+        return err
+    }
+
+    // After all activities are over, renaming the temporary path to final path
+    return os.Rename(tmpPath, finalPath)
 }
 
-func (d *Driver) Read() error {
-
+func (d *Driver) Read(collection, resource string, v interface{}) error {
+    if collection == "" {
+        return fmt.Errorf("Missing collection - no place to save record!")
+    }
+    if resource == "" {
+        return fmt.Errorf("Missing resource - unable to save record (no name)!")
+    }
+    record := filepath.Join(d.dir, collection, resource)
+    if _, err := stat(record); err != nil {
+        return err
+    }
+    b, err := os.ReadFile(record + ".json")
+    if err != nil {
+        return err
+    }
+    return json.Unmarshal(b, &v)
 }
 
-func (d *Driver) ReadAll()() {
-
+func (d *Driver) ReadAll(collection string)([]string, error) {
+    if collection == "" {
+        return nil, fmt.Errorf("Missing collection - unable to read!")
+    }
+    dir := filepath.Join(d.dir, collection)
+    if _, err := stat(dir); err != nil {
+        return nil, err
+    }
+    files, _ := os.ReadDir(dir)
+    var records []string 
+    for _, file := range files{
+        b, err := os.ReadFile(filepath.Join(dir, file.Name()))
+        if err != nil {
+            return nil, err
+        }
+        records = append(records, string(b))
+    }
+    return records, nil
 }
 
-func (d *Driver) Delete() error {
+func (d *Driver) Delete(collection, resource string) error {
+    // The resource may be optional and can be an empty string
+    // 1. If resource is passed then only that record is deleted
+    // 2. If resource is empty string, then entire dir is deleted
+    path := filepath.Join(collection, resource) 
+    mutex := d.getOrCreateMutex(collection)
+    mutex.Lock()
+    defer mutex.Unlock()
 
+    dir := filepath.Join(d.dir, path)
+
+    switch fi, err := stat(dir); {
+    case fi == nil, err != nil: 
+        return fmt.Errorf("Unable to find files or directory named %v\n", path)
+    case fi.Mode().IsDir():
+        return os.RemoveAll(dir)
+    case fi.Mode().IsRegular():
+        return os.RemoveAll(dir + ".json")
+    }
+    return nil
 }
 
-func (d *Driver) getOrCreateMutex() *sync.Mutex {
+func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
 
+    d.mutex.Lock()
+    defer d.mutex.Unlock()
+    m, ok := d.mutexes[collection]
+    if !ok {
+        m =  &sync.Mutex{}
+        d.mutexes[collection] = m
+    }
+
+    return m
+}
+
+func stat(path string)(fi os.FileInfo, err error){
+    if _, err := os.Stat(path); os.IsNotExist(err){
+        fi, err = os.Stat(path + ".json")
+    }
+    return fi, err
 }
 
 func main(){
     dir := "./"
-    db, err := new(dir, nil)
+    db, err := New(dir, nil)
     if err != nil {
         fmt.Println("Error", err)
     }
@@ -118,10 +235,13 @@ func main(){
     }
     fmt.Println(allusers)
 
-    // if err := db.Delete("user", "john"); err != nil {
+    // Delete One Document
+    // if err := db.Delete("users", "John"); err != nil {
     //     fmt.Println("Error", err)
     // }
-    // if err := db.Delete("user", "john"); err != nil {
+
+    // Delete Entire Collection
+    // if err := db.Delete("user", ""); err != nil {
     //     fmt.Println("Error", err)
     // }
 }
